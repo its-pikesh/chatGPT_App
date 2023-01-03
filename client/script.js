c69d12327f0487000bf3dd1e6f424a922595b0a4
its-pikesh <pikesh.11606271@lpu.in>
2023-01-03 13:25:20 +0530

    firebase added

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-mh3i.onrender.com/", {
     method: "post",
     headers: {
       "Content-Type": "application/json",
